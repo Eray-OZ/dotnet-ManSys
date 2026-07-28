b2af1cff31bbddeba8a0efc06dec13b2bb67c564
Eray-OZ <eray.py@gmail.com>
2026-07-28 19:30:25 +0300

    HttpClients and Policy

---
+++ api/Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Quote> Quotes { get; set; }
+    public DbSet<Coverage> Coverages { get; set; }
+
+
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coverage>()
+        .Property(c => c.Limit)
+        .HasPrecision(18,2);
+
+        modelBuilder.Entity<Quote>()
+        .Property(q => q.TotalPremium)
+        .HasPrecision(18,2);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+
+
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,58 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
-
+using Polly;
+using Polly.Extensions.Http;
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .WaitAndRetryAsync(
+            retryCount: 3,
+            sleepDurationProvider: retryAttempt =>
+                TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+        );
+}
+
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => 
+options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+
+
+
+var baseUrl = builder.Configuration["InsuranceClients:BaseUrl"];
+builder.Services.AddHttpClient("OpenCasco", client =>
+{
+    client.BaseAddress = new Uri(baseUrl!);
+    client.Timeout = TimeSpan.FromSeconds(10);
+})
+.AddPolicyHandler(GetRetryPolicy());
+
+builder.Services.AddHttpClient("TrustCasco", client =>
+{
+    client.BaseAddress = new Uri(baseUrl!);
+    client.Timeout = TimeSpan.FromSeconds(10);
+})
+.AddPolicyHandler(GetRetryPolicy());
+
+builder.Services.AddHttpClient("UnityCasco", client =>
+{
+    client.BaseAddress = new Uri(baseUrl!);
+    client.Timeout = TimeSpan.FromSeconds(10);
+})
+.AddPolicyHandler(GetRetryPolicy());
+
 
 
 var app = builder.Build();
@@ -12,6 +61,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
+
+
 
 app.MapControllers();
 app.UseHttpsRedirection();
